412799a78d3e4dda4ced23e8c4a6a978d34e9876
Subrahmanya Gonella <satishgonella2024@gmail.com>
2024-12-13 12:34:25 +0000

    Added provider block for AWS

--- main.tf
+++ main.tf
@@ -7,7 +7,19 @@ terraform {
     region         = "eu-west-2"
     dynamodb_table = "terraform-locks"
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
 
 module "network" {
   source = "./modules/network"
